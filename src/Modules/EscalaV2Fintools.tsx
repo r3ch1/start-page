5c60470303e9c46795b7f566d45b2b0a74e9ded7
Heric Branco <hericbranco@gmail.com>
2024-08-01 18:56:16 -0300

    feat - imrove user selection and add cache

--- src/Modules/EscalaV2Fintools.tsx
+++ src/Modules/EscalaV2Fintools.tsx
@@ -3,33 +3,32 @@ import { useEffect, useState } from 'react';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 
 const EscalaV2Fintools = () => {
-  const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
   const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
   const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
   const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
-  const [users, setUsers] = useState([{ name: '', selected: false }]);
+  const [users, setUsers] = useState([{ name: '', selected: false, index: 0 }]);
   const [data, setData] = useState([]) as any[];
   const [dataFull, setDataFull] = useState([]) as any[];
 
-  const initializeGapiClient = async () => {
-    await gapi.client.init({
-      apiKey: API_KEY,
-      discoveryDocs: [DISCOVERY_DOC],
-    });
+  const getFromGoogleOrStorage = async (forceReload = false) => {
+    try {
+      let escalaFull = JSON.parse(localStorage.getItem('escalaFull') || '{}');
+      if (!Object.keys(escalaFull).length) {
+        const response = await window.gapi.client.sheets.spreadsheets.values.get({
+          spreadsheetId: process.env.REACT_APP_ESCALA_SHEET_ID,
+          range: "'escala RJ'!A1:ZZ",
+        });
+        escalaFull = response.result;
+        localStorage.setItem('escalaFull', JSON.stringify(escalaFull));
+      }
+      return escalaFull;
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   const getDados = async () => {
-    let response;
-    try {
-      response = await window.gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: process.env.REACT_APP_ESCALA_SHEET_ID,
-        range: "'escala RJ'!A1:ZZ",
-      });
-    } catch (err) {
-      console.log(err);
-      return;
-    }
-    const range = response.result;
+    const range = await getFromGoogleOrStorage();
     if (!range || !range.values || range.values.length === 0) {
       console.log('sem dados');
       return;
@@ -37,8 +36,8 @@ const EscalaV2Fintools = () => {
     setUsers(
       range.values[0]
         .filter((item: any) => item.trim() !== '' && item !== 'Total do dia')
-        .map((item: any) => {
-          return { name: item, selected: false };
+        .map((item: any, i: number) => {
+          return { name: item, selected: false, index: i + 1 };
         }),
     );
     setDataFull(range.values);
@@ -58,7 +57,7 @@ const EscalaV2Fintools = () => {
       await getDados();
     };
 
-    if (gapi.client.getToken() === null) {
+    if (window.gapi.client.getToken() === null) {
       tokenClient.requestAccessToken({ prompt: 'consent' });
     } else {
       tokenClient.requestAccessToken({ prompt: '' });
@@ -72,15 +71,22 @@ const EscalaV2Fintools = () => {
     }
     const index = users.indexOf(selectedUser);
     const _users = users.map((user: any) => {
-      return { name: user.name, selected: false };
+      return { name: user.name, selected: false, index: user.index };
     });
-    selectedUser.selected = !selectedUser.selected;
-    _users.splice(index, 1);
-    setUsers([...[selectedUser], ..._users]);
+    _users[index].selected = !selectedUser.selected;
+    setUsers(JSON.parse(JSON.stringify(_users)));
   };
 
   useEffect(() => {
-    gapi.load('client', initializeGapiClient);
+    const initializeGapiClient = async () => {
+      await window.gapi.client.init({
+        apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+      });
+    };
+    console.log('CARREGA');
+    window.gapi.load('client', initializeGapiClient);
+    getDados();
   }, []);
 
   useEffect(() => {
@@ -91,7 +97,7 @@ const EscalaV2Fintools = () => {
     dataFull.forEach((item: any) => {
       if (item.length > userIndex && item.length > 1 && item[userIndex].toLocaleLowerCase() === 'x') {
         presencialInWeek.push({
-          date: moment(item[0], 'DD/M').format('YYYY-MM-DD'),
+          date: moment(item[0], 'D/M').format('YYYY-MM-DD'),
           isOffice: true,
         });
       }
@@ -118,7 +124,7 @@ const EscalaV2Fintools = () => {
                 value={user.name}
                 onChange={(e) => setChecked(e.currentTarget.value)}
               >
-                {user.name}
+                {user.name}[{user.index}]
               </ToggleButton>
             </>
           ))}
@@ -127,6 +133,7 @@ const EscalaV2Fintools = () => {
         return (
           <div className={moment().day(day).isSame(moment()) ? 'col bg-primary' : 'col'} key={`escala-days-${day}`}>
             <p>{moment().day(day).format('DD/MM')}</p>
+            <p>{moment().day(day).format('ddd')}</p>
             {data.length !== 0 && (
               <p>
                 {data.find((item: any) => item.date === moment().day(day).format('YYYY-MM-DD')) !== undefined
@@ -137,6 +144,23 @@ const EscalaV2Fintools = () => {
           </div>
         );
       })}
+      {dataFull
+        .filter(
+          (item: any, i: number) => i > 1 && item.length && moment().add(1, 'weeks').day(1).format('D/M') === item[0],
+        )
+        .map((data: any, i: number) => {
+          return (
+            <div className="col" key={`escala-days-${i}`}>
+              <p>{data[0]}</p>
+              <p>{moment(data[0], 'D/M').format('ddd')}</p>
+              <p>
+                {data[users.find((item: any) => item.selected)?.index || 0].toLocaleLowerCase() === 'x'
+                  ? 'OFFICE'
+                  : 'HOME'}
+              </p>
+            </div>
+          );
+        })}
       {!data.length && (
         <div className="col-12">
           <button className="btn btn-primary w-100" onClick={() => handleAuthClick()}>
