c3072fa64ef3d99137ff5f7bb0cd5c6f8825808a
Heric Branco <hericbranco@gmail.com>
2024-08-07 10:40:54 -0300

    feat - add a simple login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
+import Login from './Components/Login';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         {/* <Route path="about" element={<About />} /> */}
       </Routes>
     </Router>
